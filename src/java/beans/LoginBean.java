d99f107ef044f969f9935159cd922496ec49dd0d
TakafumiSato <sato@augusta-inc.co.jp>
2018-10-24 09:08:46 +0900

    logout

--- src/java/beans/LoginBean.java
+++ src/java/beans/LoginBean.java
@@ -41,6 +41,7 @@ public class LoginBean {
 
     
     public LoginBean() {
+        
         userTC = new UserTableController();
     }
     
---
+++ src/java/beans/LogoutBean.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import data.UserData;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ * @author 佐藤孝史
+ */
+@ManagedBean
+@RequestScoped
+public class LogoutBean {
+    
+    @ManagedProperty(value="#{userData}")
+    private UserData userData;
+
+    /**
+     * Creates a new instance of LogoutBean
+     */
+    public LogoutBean() {
+    }
+
+    public void setUserData(UserData userData) {
+        this.userData = userData;
+    }
+    
+    public String logout() {
+        
+        FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+        
+        return "login.xhtml?faces-redirect=true";
+    }
+}
